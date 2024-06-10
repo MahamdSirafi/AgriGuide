3032e33706be7b3f5699d30483abed83df68ece8
adel <maszzzzy@gmail.com>
2024-06-10 19:44:48 +0200

    fix <delete table>

--- models/storageModel.js
+++ models/storageModel.js
@@ -1,19 +1,19 @@
-const mongoose = require("mongoose");
-const storageSchema = new mongoose.Schema({
-   
-    name : {
-      required : [true , 'must enter name'],
-      type : String,
+const mongoose = require('mongoose');
+const storageSchema = new mongoose.Schema(
+  {
+    name: {
+      required: [true, 'must enter name'],
+      type: String,
     },
-    discrption : {
-      required : [true , 'must enter discrption'],
-      type : String,
+    discrption: {
+      required: [true, 'must enter discrption'],
+      type: String,
     },
-    
-},{
-      timestamps: true,
-      versionKey: false
-    });
-    const Storage = mongoose.model("Storage", storageSchema);
-    module.exports = Storage;
-    
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+const Storage = mongoose.model('Storage', storageSchema);
+module.exports = Storage;
--- routes/storageRouter.js
+++ routes/storageRouter.js
@@ -1,13 +1,15 @@
-const storageController = require("../controllers/storageController");
-  const authMiddlewers = require('./../middlewares/authMiddlewers');
-  const express = require("express");
-  const router = express.Router();
-  router.use(authMiddlewers.protect);
-  router.route("/").get(storageController.getAllstorage).post(storageController.createstorage);
-  router
-    .route("/:id")
-    .get(storageController.getstorage)
-    .patch(storageController.updatestorage)
-    .delete(storageController.deletestorage);
-  module.exports = router;
-  
+const storageController = require('../controllers/storageController');
+const authMiddlewers = require('./../middlewares/authMiddlewers');
+const express = require('express');
+const router = express.Router();
+router.use(authMiddlewers.protect);
+router
+  .route('/')
+  .get(storageController.getAllstorage)
+  .post(storageController.createstorage);
+router
+  .route('/:id')
+  .get(storageController.getstorage)
+  .patch(storageController.updatestorage)
+  .delete(storageController.deletestorage);
+module.exports = router;
